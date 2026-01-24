f51ed238f298001a9e7db102c013cb662bac48b5
CapyBlaze <capyblaze@gmail.com>
2026-01-24 11:43:00 +0100

    :tada: Begin a file picker component and a compression functionality.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import FilePicker from "./Components/filePicker"
+
+function App() {
+
+    return (
+        <>
+            <div className="component">
+                <div className="w-1/2">
+                    <h1><span className="text-white font-semibold">Check-Mate</span><span className="text-[#734c29]">Data</span></h1>
+                    <FilePicker></FilePicker>
+                </div>
+                <div className="w-1/2">
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default App
